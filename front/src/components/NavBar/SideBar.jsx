7e396d87a30f6630e4d95526bd4b12418a020cae
robertpetre <90182995+robertpetre@users.noreply.github.com>
2023-12-09 22:55:19 +0200

    fixes

--- front/src/components/NavBar/SideBar.jsx
+++ front/src/components/NavBar/SideBar.jsx
@@ -1,39 +1,53 @@
-import { Link } from "react-scroll"
-import "../../index.css"
-
+import { Link } from "react-scroll";
+import "../../index.css";
 
 function SideBar(props) {
+  const userToken = localStorage.getItem("userToken");
+
   return (
     <div className="fixed flex flex-col z-40 bg-slate-900 text-white top-20 right-0 rounded-lg">
-       <div className="flex flex-col items-center justify-center">
-    <Link
-        activeClass="active"
-        to="research-section"
-        spy={true}
-        smooth={true}
-        offset={-50}
-        duration={500}
-        className="nav-link cursor-pointer rounded-lg hover:scale-105 transition-all duration-300 text-md p-4"
-        onClick={props.toggleDropdown}
-    >
-        Login
-    </Link>
-    <Link
-      activeClass="active"
-      to="awards-section"
-      spy={true}
-      smooth={true}
-      offset={-50}
-      duration={500}
-      className="nav-link cursor-pointer rounded-lg hover:scale-105 transition-all duration-300 text-md p-4"
-      onClick={props.toggleDropdown}
-    >
-        Register
-    </Link>
-  </div>
+      <div className="flex flex-col items-center justify-center">
+        {userToken ? (
+          <Link
+            to="dashboard"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
+            className="nav-link cursor-pointer rounded-lg hover:scale-105 transition-all duration-300 text-md p-4"
+            onClick={props.toggleDropdown}
+          >
+            Dashboard
+          </Link>
+        ) : (
+          <>
+            <Link
+              to="research-section"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={500}
+              className="nav-link cursor-pointer rounded-lg hover:scale-105 transition-all duration-300 text-md p-4"
+              onClick={props.toggleDropdown}
+            >
+              Login
+            </Link>
+            <Link
+              to="awards-section"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={500}
+              className="nav-link cursor-pointer rounded-lg hover:scale-105 transition-all duration-300 text-md p-4"
+              onClick={props.toggleDropdown}
+            >
+              Register
+            </Link>
+          </>
+        )}
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
